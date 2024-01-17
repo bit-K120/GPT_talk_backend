57dca75ed154985ceddec559df7993b97edad5af
bit-K120 <kayugen105@gmail.com>
2024-01-17 14:58:13 +0900

    activated Perplexity

--- chatapp/main_output_Mi7B.py
+++ chatapp/main_output_Mi7B.py
@@ -1,7 +1,8 @@
 import os
+import requests
 from dotenv import load_dotenv
-import openai
 from .tts_config import create_text_to_speech
+from . import global_state
 
 
 def AI_chat_Mi7B(voice_input):
@@ -16,35 +17,43 @@ def AI_chat_Mi7B(voice_input):
         print(f"You:{user_input}")
     else: 
         print("user_inputを受け取っていません")    
-            # perplexityのメッセージ設定
-    messages = [
-        {
-        "role": "system",
-        "content": (
-            "You are a friendly English teacher, and you need to "
-            "engage in a friendly conversation with user."
-            "Your response has to be less than 15 words."
-
-
-        ),
-        },
-        {
-        "role": "user",
-        "content": (user_input),
-        },
-        ]
-        # aiの設定
-
-    response = openai.ChatCompletion.create(
-        model="mistral-7b-instruct",
-        messages=messages,
-        api_base="https://api.perplexity.ai",
-        api_key=PERPLEXITY_API_KEY,   
-        )                                           
-                      
-        # aiから生成されたものから文字部分のみ抽出
-    gpt_response = response.choices[0].message.content
-    print(f"AI:{gpt_response}")
-    text_to_speech_data = create_text_to_speech(gpt_response)
     
+    # ----------------------- perplexityAI api の設定 ---------------------------
+    url = "https://api.perplexity.ai/chat/completions"
+    ai_prompt = "You are a friendly language teacher. Your response must be less than 50 words."
+    if global_state.mode_selection == "Translate":
+        ai_prompt = f"Translate the given input to {global_state.out_language_selection}. Do not include anything else."    
+    payload = {
+        "model": "llama-2-70b-chat",
+        "messages": [
+            {
+                "role": "system",
+                "content": ai_prompt
+            },
+            {
+                "role": "user",
+                "content": user_input
+            }
+        ],
+        "max_tokens":150
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": f"Bearer {PERPLEXITY_API_KEY}"
+    }
+   
+    gpt_response = None
+    text_to_speech_data = None
+    response = requests.post(url, json=payload, headers=headers)
+    if response.status_code == 200:
+        # aiから生成されたものから文字部分のみ抽出
+        response_json = response.json()
+        gpt_response = response_json["choices"][0]["message"]["content"]
+        print(f"AI:{gpt_response}") 
+        text_to_speech_data = create_text_to_speech(gpt_response)
+    else:
+        print(f"Error: Received status code {response.status_code}")  
+                             
     return gpt_response, text_to_speech_data
+ 
